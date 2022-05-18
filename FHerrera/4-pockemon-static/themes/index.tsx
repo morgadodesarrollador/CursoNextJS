cb8faf24c13b2348081ff00017d6142aad6aa7d8
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-05-18 13:01:27 +0200

    4.2 Tema Dark

---
+++ FHerrera/4-pockemon-static/themes/index.tsx
@@ -0,0 +1,4 @@
+//Archivo de exportación de todas las funcionalidades dentro de la carpeta thems
+//Realizará la exportación de todos los temas que vayamos creando y después actualizarlos donámicamente
+//se les conocen como archivos de Barril
+export * from './darktheme';
