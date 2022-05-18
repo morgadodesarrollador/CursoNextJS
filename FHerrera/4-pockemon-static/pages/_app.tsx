b1ab8edda7b0c42c35f006e75c7a450fdeaf2617
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-05-18 12:14:06 +0200

    4.1-Next _document.js with CssBaseline

---
+++ FHerrera/4-pockemon-static/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app'
+import { NextUIProvider } from '@nextui-org/react';
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  )
+}
+
+export default MyApp
---
+++ FHerrera/4-pockemon-static/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@nextui-org/react'
+import type { NextPage } from 'next'
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Button color="success"> Hola Mundo</Button>
+    </>
+  )
+}
+
+export default HomePage
