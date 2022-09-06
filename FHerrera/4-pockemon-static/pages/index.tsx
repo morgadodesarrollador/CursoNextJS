5a0bcaade533263e85827d731a063627cb08c775
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-09-06 22:14:03 +0200

    GetStaticProps

--- FHerrera/4-pockemon-static/pages/index.tsx
+++ FHerrera/4-pockemon-static/pages/index.tsx
@@ -1,13 +1,37 @@
-import { Button } from '@nextui-org/react'
-import type { NextPage } from 'next'
+// import { Button } from '@nextui-org/react'
+import type { NextPage, GetStaticProps } from 'next'
 import { Layout } from '../components/layouts/index';
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log(props);
   return (
     <Layout title='Listado de Pokemons'>
-      <Button color="success"> Hola Mundo</Button>
+      <ul>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>...</li>
+      </ul>
     </Layout>
   )
 }
-
+/*  se usa cuando la data requerida está disponible en build time 
+(tiempo de construcción de la aplicación). Se renderiza antes de hacer
+el request de la página
+*/
+export const getStaticProps: GetStaticProps = async(ctx) => {
+  // const { data } = await
+  console.log('Hola Mundo');
+  return { 
+    props: {
+      name: 'Morgado'
+    }
+  }
+}
+// export async function getStaticProps(context) {
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   }
+// }
 export default HomePage
