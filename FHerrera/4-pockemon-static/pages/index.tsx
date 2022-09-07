ae976029c64dde4c097090a09caa6c2403b18ea7
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-09-07 21:56:48 +0200

    48-Listar id y nombres

--- FHerrera/4-pockemon-static/pages/index.tsx
+++ FHerrera/4-pockemon-static/pages/index.tsx
@@ -1,19 +1,28 @@
 // import { Button } from '@nextui-org/react'
+import { Grid } from '@nextui-org/react';
 import type { NextPage, GetStaticProps } from 'next'
 import { pokeApi } from '../api';
 import { Layout } from '../components/layouts/index';
-import { PokemonsListResponse } from '../interfaces/pokemons-list';
+import { PokemonsListResponse, SmallPokemon } from '../interfaces/pokemons-list';
 
-const HomePage: NextPage = (props) => {
-  console.log(props);
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+//https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg ← ID
+
+const HomePage: NextPage<Props> = ({ pokemons }) => {
+  console.log(pokemons);
   return (
     <Layout title='Listado de Pokemons'>
       <ul>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>Pokemon</li>
-        <li>...</li>
+        {
+          pokemons.map ( ({ id, name }) => (
+            <li key={id}>
+              #{ id } - { name }
+            </li>
+          ))
+      }
       </ul>
     </Layout>
   )
@@ -23,12 +32,22 @@ const HomePage: NextPage = (props) => {
 el request de la página
 */
 export const getStaticProps: GetStaticProps = async(ctx) => {
-  // const { data } = await
   const { data } = await pokeApi.get<PokemonsListResponse>('/pokemon?limit=151');
   console.log( data );
+
+  // la idea es devolver un array con id, nombre, imagen y url
+  //map regresa un nuevo array con el retorno de la funcion de flecha que hay dentro
+  // ({ ... }) --> el () es para hacer implícito el return del objeto que devuelve la función
+  const pokemons: SmallPokemon[] = data.results.map( (poke, idx) => ({ 
+    ...poke, //destructuramos el objeto (name y url)
+    id: idx + 1, 
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${idx+1}.svg`
+  })); 
+  
   return { 
     props: {
-      pokemons: data.results //hemos de crear la interface y definir un tipo estricto
+      // pokemons: data.results //lo que devuelve la API
+      pokemons: pokemons 
     }
   }
 }
