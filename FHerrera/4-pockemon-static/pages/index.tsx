a89e4563cae02e7aae52411911bf01786bde0ddf
morgadodesarrollador <34490065+morgadodesarrollador@users.noreply.github.com>
2022-09-07 22:43:07 +0200

    49-Mostrar tarjetas con imagen, id y nombre usando componentes de nextui

--- FHerrera/4-pockemon-static/pages/index.tsx
+++ FHerrera/4-pockemon-static/pages/index.tsx
@@ -1,5 +1,5 @@
 // import { Button } from '@nextui-org/react'
-import { Grid } from '@nextui-org/react';
+import { Card, Grid, Row, Text } from '@nextui-org/react';
 import type { NextPage, GetStaticProps } from 'next'
 import { pokeApi } from '../api';
 import { Layout } from '../components/layouts/index';
@@ -15,15 +15,25 @@ const HomePage: NextPage<Props> = ({ pokemons }) => {
   console.log(pokemons);
   return (
     <Layout title='Listado de Pokemons'>
-      <ul>
+      <Grid.Container gap={ 2 } justify='flex-start'>
         {
-          pokemons.map ( ({ id, name }) => (
-            <li key={id}>
-              #{ id } - { name }
-            </li>
+          pokemons.map ( ({ id, name, img }) => (
+            <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+              <Card hoverable clickable>
+                <Card.Body css={ {p:1}}>
+                  <Card.Image src= {img} width="100%" height={ 140 } />
+                  <Card.Footer>
+                    <Row justify="space-between">
+                      <Text transform='capitalize'>{name}</Text>
+                      <Text>#{id}</Text>
+                    </Row>
+                  </Card.Footer>
+                </Card.Body>
+              </Card>  
+            </Grid>
           ))
       }
-      </ul>
+      </Grid.Container>
     </Layout>
   )
 }
